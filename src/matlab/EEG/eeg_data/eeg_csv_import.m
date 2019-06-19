237aa67ccf03d07305d6fb928ba86532bc0bad63
utlandr <rbel068@aucklanduni.ac.nz>
2019-06-19 12:30:32 +1200

    Transpose for EEGLab input

--- src/matlab/EEG/eeg_data/eeg_csv_import.m
+++ src/matlab/EEG/eeg_data/eeg_csv_import.m
@@ -7,6 +7,6 @@ opts = detectImportOptions("pre-training//P1_RW(2019.06.05-11.12.46)__Pre1.csv")
 opts.SelectedVariableNames = opts.SelectedVariableNames(3:18);
 opts.ExtraColumnsRule = 'ignore';
 
-% View and read matrix.
+% View and read matrix (trnaspose for eeglab import).
 preview('pre-training/P1_RW(2019.06.05-11.12.46)__Pre1.csv', opts)
-eeg_data_test = readmatrix('pre-training/P1_RW(2019.06.05-11.12.46)__Pre1.csv', opts);
+eeg_data_test = transpose(readmatrix('pre-training/P1_RW(2019.06.05-11.12.46)__Pre1.csv', opts));
