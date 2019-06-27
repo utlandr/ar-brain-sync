7de5d02b6641f5ade0f662170d59c7f6997b3129
Reed Bell <rbell11235@hotmail.co.nz>
2019-06-27 13:20:54 +1200

    Implemented Single sample Phase Locking Value algorithm (SPLV). Currently untested.

---
+++ src/matlab/EEG/phase_locking_value/splv.m
@@ -0,0 +1,72 @@
+function [plv] = splv(eeg_data, srate, freq_band, order, delta_half)
+    % DOCSTRING
+    % This is an implementation of the Phase Locking Value (PLV) algorithm that
+    % only considers a single sample/trial to compute PLVs. It varies from the 
+    % traditional implementation which averages over the samples, by averaging
+    % over a time window. This implmentation is similar to pn_eegPLV
+    %
+    % INPUT
+    % eeg_data      -   (nxmx2 double)A 3D matrix with dimensions nchannels 
+    %                   x ntimepoints x ntrials = 2 of EEG readings.
+    % srate         -   (int) The sampling rate of the EGG data in Hz
+    % freq_band     -   (2x1 double) The range of frequencies to calculate PLV
+    %                   for (e.g. use [8 12] for alpha waves.
+    % order         -   The order of the FIR filter (should be designed to
+    %                   approsimately 4-5 cycles of the desired frequency. e.g
+    %                   alpha wave has frequency of 10Hz (100ms) with a srate
+    %                   of 125Hz (8ms) would use an order = 50 so that 8*50 =
+    %                   400ms/100ms = 4 cycles
+    %
+    % OUTPUT
+    % plv           -   (nxmx2 double) A 3D matrix with dimensions nchannels x
+    %                   ntimepoints x ntrials = 2 containing calculated PLVs
+    %
+    % AUTHOR
+    % Reed Bell     -   rbel068@aucklanduni.ac.nz
+    
+    % First, recieve information about channel number and timepoints
+    [n_channels, n_timepoints, ~] = size(eeg_data);
+    
+    % Specify FIL filter for specific frequency range
+    fil = fir1(order, 2/srate*freq_band);
+                    
+    % Apply the FIL filter
+    fil_eeg = filter(fil, 1, eeg_data, [], 2);
+    fil_hilbert_eeg = fil_eeg;
+    
+    % Apply Hilbert transformation to find instantaneous phase at each
+    % timepoint
+    for k = 1:2 
+        for channel = 1:n_channels
+            tmp_hilbert = hilbert(fil_eeg(channel, :, k));
+            fil_hilbert_eeg(channel, :, k) = angle(tmp_hilbert);
+            
+        end
+    end
+    
+    % Calculate time averaged PLV between all possible channel pairs
+    plv = zeros(n_channels, n_channels, n_timepoints);
+    
+    for chan_1 = 1:n_channels 
+        for chan_2 = 1:n_channels
+            for t = 1:n_timepoints
+                if t - delta_half + 1 < 1
+                    window = 1:t+delta_half;
+                    
+                elseif t + delta_half > n_timepoints
+                    window = t-delta_half+1:n_timepoints;
+                    
+                else
+                    window = t-delta_half+1:t+delta_half;
+                    
+                end
+                
+                single_plvs = exp(1i*(fil_hilbert_eeg(chan_1, window, 1) - fil_hilbert_eeg(chan_2, window, 2)));
+                plv(chan_1, chan_2, t) = abs((1 / (2 * delta_half))*sum(single_plvs));
+            
+            end
+        end
+    end
+    
+    
+    return
